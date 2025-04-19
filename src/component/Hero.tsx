6c90e5b45529313f9a53b733e2463b07c625ab5b
sunil sangle <yes.sangle.com>
2025-04-19 16:18:04 +0530

    men, women category and video wth img slider added

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,17 +1,71 @@
-
+import { Carousel } from "react-bootstrap";
+import { useEffect, useRef } from "react";
 
 const Hero: React.FC = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const video = videoRef.current;
+                if (!video) return;
+
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        video.play().catch(() => { });
+                    } else {
+                        video.pause();
+                    }
+                });
+            },
+            { threshold: 0.2 }
+        );
+
+        const video = videoRef.current;
+        if (video) observer.observe(video);
+
+        return () => {
+            if (video) observer.unobserve(video);
+        };
+    }, []);
+
     return (
-        <>
-            <section className="hero">
-                <div>
-                    {/* <h1>Fragrance</h1>
-                    <p className="lead">The Griffin Original</p> */}
-                </div>
-            </section>
+        <section className="hero">
+            <Carousel controls={false} indicators={false} fade interval={6000}>
+                {/* Slide 1 - Video */}
+                <Carousel.Item>
+                    <video
+                        ref={videoRef}
+                        className="d-block w-100"
+                        muted
+                        loop
+                        playsInline
+                        style={{ objectFit: "cover", height: "100vh" }}
+                    >
+                        <source src="./src/assets/images/intro.mp4" type="video/mp4" />
+                    </video>
+                </Carousel.Item>
 
-        </>
-    )
-}
+                {/* Slide 2 - Image */}
+                <Carousel.Item>
+                    <img
+                        src="./src/assets/images/p8.jpg"
+                        className="d-block w-100"
+                        alt="Perfume Slide"
+                    />
+                </Carousel.Item>
 
-export default Hero
+                {/* Slide 3 - Image */}
+                <Carousel.Item>
+                    <img
+                        src="./src/assets/images/banner-1.jpg"
+                        className="d-block w-100"
+                        alt="Perfume Slide 2"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </section>
+    );
+};
+
+export default Hero;
--- src/component/WomenCategory.tsx
+++ src/component/WomenCategory.tsx
@@ -1,11 +1,92 @@
 const WomenCategory: React.FC = () => {
     return (
-      <div className="container" style={{'height': '50vh'}}>
-      <h1>
-      WomenCategory...
-      </h1>
-      
+      <div className="container">
+      <h4 className="text-center pt-5 pb-3">
+        Categories
+      </h4>
+      <div className="mens-wrap">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/new_arrival1.png" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Men</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/slide4.png" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Party & Night Out</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/w-1.webp" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Date Wear</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/p2.jpg" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Office Fragrances</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/p3.jpg" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Travel Fragrances</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="face face1">
+                <div className="content">
+                  <img src="../../src/assets/images/p6.jpg" alt="" className="img-fluid" />
+                </div>
+              </div>
+              <div className="face face2">
+                <h2>Exotic Oudhs</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="py-3 mt-4"></hr>
+        <div className="row">
+          <img src="../../src/assets/images/pdetails_img.png" alt="" className="img-fluid" />
+        </div>
       </div>
+    </div>
     )
   }
   
